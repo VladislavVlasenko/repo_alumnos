6f0e21ad884d64dfa8ef38d83c9b9fe333d97f5d
Vladislav <vlasenko69@gmail.com>
2016-02-06 22:12:31 +0100

    cambios en alumnos.cs

---
+++ Alumnos/Alumnos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Alumnos
+{
+    class Alumnos
+    {
+        ///<summary>
+        ///Se crea un arraylist para meter los alumnos..
+        ///</summary>
+        
+        private ArrayList listaAlumnos = new ArrayList();
+
+        public ArrayList ListaAlumnos 
+        {
+            get { return listaAlumnos; }
+        }
+
+        ///<summary>
+        ///Metodo para agregar los alumnos en el arraylist..
+        ///</summary>
+        ///<param name="Alum">Alumno que se le pasa</param>
+        public void agregarAlumno(Alumno Alum)
+        {
+            listaAlumnos.Add(Alum);
+        }
+        ///<summary>
+        ///metodo para sacar la media de los alumnos...
+        ///</summary>
+        ///<returns>Devuelve la media de los alumnos </returns>
+
+        public double medianotas()
+        {
+            int acumulador = 0;
+            double medialumnos = 0;
+
+            foreach (Alumno alum in listaAlumnos)
+            {
+                acumulador = acumulador + alum.Nota;
+            }
+
+            if (listaAlumnos.Count == 0)
+            {
+                medialumnos = 0;
+            }
+            else
+            {
+                medialumnos = medialumnos / listaAlumnos.Count;
+            }
+
+            return acumulador;
+        }
+
+    }
+
+}
---
+++ Alumnos/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alumnos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre, nota, texto;
+            
+            nombre = textBox1.Text;
+            nota = textBox2.Text;
+            
+
+            texto = nombre + " " + "Nota: " + nota;
+            textBox3.Text = textBox3.Text + texto + "\n";
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+
+        }
+    }
+}
